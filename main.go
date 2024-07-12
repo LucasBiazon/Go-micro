8abbdb25d5aea4d2652d44d7c1addd48309019bb
Lucas <lucasbiazonpalma@gmail.com>
2024-07-12 00:29:42 -0300

    Feat: add route handler

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
-	"go-micro.dev/v4"
+	"go-micro.dev/v5/web"
 )
 
 func main() {
-	service := micro.NewService(
-		micro.Name("hello-world"),
-		micro.Address(":8080"),
+	service := web.NewService(
+		web.Name("hello-world"),
+		web.Address(":8080"),
 	)
 
 	service.Init()
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World!")
+	})
+
+	service.Handle("/", http.DefaultServeMux)
 
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
